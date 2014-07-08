c1a5b00a9b6f0b9baacaccd56a321f87c8855e16
Max Malakhov <malakhovbox@gmail.com>
2014-07-08 19:06:03 +0400

    YBS-2 Added REST router

---
+++ routes/rest.js
@@ -0,0 +1,24 @@
+/**
+ * Created by max on 7/8/14.
+ */
+var express = require('express');
+var ball = require('../controllers/ball-controller');
+
+var router = express.Router();
+
+/* Rest for Mobile requests. */
+router.get('/', function(req, res) {
+    res.send('REST. Respond with a resource');
+});
+router.route('/ball')
+    .get(function(req, res) {
+
+        res.send('REST. Ball was gotten successfully');
+    })
+    .post(function(req, res) {
+
+    res.json({ message: 'REST. Ball was posted successfully'});
+});
+
+
+module.exports = router;
